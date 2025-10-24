c6a216a0dfa12cea27c76ef0c460549b5febff41
Yovadi <105678204+Yovadi@users.noreply.github.com>
2025-10-24 20:45:04 +0200

    Updated pdfGenerator.ts

--- src/utils/pdfGenerator.ts
+++ src/utils/pdfGenerator.ts
@@ -154,7 +154,7 @@ async function buildInvoicePDF(pdf: jsPDF, invoice: InvoiceData) {
   }
 
   yPosition = 57;
-  const invoiceInfoCol = pageWidth - margin - 45;
+  const invoiceInfoCol = pageWidth - margin - 55;
 
   pdf.setFontSize(8);
   pdf.setFont('helvetica', 'bold');
